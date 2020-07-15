0f15f24f36ec725ff6979720e6d12d291922da03
Greg McGuire <mcguireg@umich.edu>
2020-07-15 15:23:43 -0400

    Update path follower example to support websockets, correct several endpoint bugs

--- octane-waypoint-follower/follow-path.py
+++ octane-waypoint-follower/follow-path.py
@@ -6,6 +6,7 @@ from abc import ABC
 
 import geojson
 import requests
+import socketio
 from pyproj import Geod
 
 """
@@ -28,13 +29,47 @@ class OctaneInstance:
 
     TODO: this should go away in lieu of a more full-featured API implementation
     """
-    def __init__(self, auth_token, api_base_url="https://mvillage.um.city/api"):
-        self.api_base_url = api_base_url
+    def __init__(self, auth_token, api_server="https://octane.mvillage.um.city"):
+        self.auth_token = auth_token
+        self.api_server = api_server
+
+        self.api_base_url = f"{api_server}/api"
         self.session = requests.Session()
         self.session.headers = {'X-API-KEY': auth_token}
+        self.socket = socketio.Client()
+        self.socket.register_namespace(self.OctaneNamespace(self))
+
+    def __enter__(self):
+        # Connect to websocket
+        self.socket.connect(self.api_server, transports=None, namespaces=[self.OctaneNamespace.namespace])
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.socket.disconnect()
+
+    def emit(self, channel, payload):
+        self.socket.emit(channel, payload, namespace=self.OctaneNamespace.namespace)
 
     def post_json_message(self, endpoint, json_message):
         return self.session.post("%s%s" % (self.api_base_url, endpoint), json=json_message)
+
+    def get(self, endpoint):
+        return self.session.get("%s%s" % (self.api_base_url, endpoint))
+
+    class OctaneNamespace(socketio.ClientNamespace):
+        namespace = "/octane"
+
+        def __init__(self, octane_instance):
+            self.octane_instance = octane_instance
+            super().__init__(self.namespace)
+
+        def on_connect(self):
+            self.octane_instance.socket.emit('auth', {'x-api-key': self.octane_instance.auth_token}, 
+                                             namespace=self.namespace)
+
+        def on_disconnect(self):
+            self.octane_instance.socket.disconnect()
 
 
 class MovingPoint(geojson.Point):
@@ -46,12 +81,16 @@ class MovingPoint(geojson.Point):
         self.heading = heading
         super().__init__(**extra)
 
+    def __str__(self):
+        return f"MovingPoint: lat {self.coordinates[1]:>-3.9f}, lon {self.coordinates[0]:>-3.9f}, {self.speed:>3.1f}m/s, heading {self.heading:03.2f}" 
+
 
 class V2XMessageType(ABC):
     """
     Types of V2X messaging to support, per SAE J2735
     """
     frequency_hz = 1
+    protocol = "J2735_201603"
 
     @property
     def time_per_msg_s(self):
@@ -71,19 +110,28 @@ class V2XMessageType(ABC):
         """
         pass
 
+    @property
+    def name(self):
+        """
+        Message type that this class supports. Corresponds to Octane's "messageType" field
+        """
+        pass
+
 
 class BSM(V2XMessageType):
     """
     Basic Safety Message
     """
     frequency_hz = 10
-    temp_id = 0
+    id = "000003B5" # 949 for now, in the Mcity range. TODO: allow setting this
 
     # TODO: allow initialization of these
     _base_params = {
-        "id": "PathFollower 0.1",
-        "idFixed": "1212",
-        "messageSet": "J2735_200612",
+        "id": id,
+        "idTemporary": id,
+        "idFixed": id,
+        "angle": 0.0, # TODO: compute!
+        "messageSet": V2XMessageType.protocol,
         "vehicleLength": 4.5,
         "vehicleWidth": 1.83,
     }
@@ -92,11 +140,12 @@ class BSM(V2XMessageType):
         # Assumes OCTANE of course!
         return "/v2x/rsu/%s/bsm" % rsu_id
 
-    def as_message(self, moving_point):
-        self.temp_id += 1
+    @property
+    def name(self):
+        return "BSM"
 
+    def as_message(self, moving_point):
         message = {
-            "idTemporary": self.temp_id,
             "longitude": moving_point.coordinates[0],
             "latitude": moving_point.coordinates[1],
             "elevation": moving_point.coordinates[2] if len(moving_point.coordinates) > 2 else 0,
@@ -118,6 +167,10 @@ class PSM(V2XMessageType):
         # Assumes OCTANE of course!
         return "/v2x/rsu/%s/psm" % rsu_id
 
+    @property
+    def name(self):
+        return "PSM"
+
     def as_message(self, moving_point):
         raise NotImplementedError
 
@@ -132,17 +185,33 @@ class PathFollower(ABC):
     """
     geod = Geod(ellps="WGS84")
 
-    def __init__(self, geojson_filename, velocity_ms):
+    def __init__(self, geojson_filename, velocity_meters_per_s):
         """
 
         :param geojson_file: A geojson file that contains a Feature with a LineString
-        :param velocity_ms: Object velocity in meters/sec
+        :param velocity_meters_per_s: Object velocity in meters/sec
         """
         with open(geojson_filename) as geojson_file:
             geo_feature = geojson.load(geojson_file)
 
         self.geojson_path = geojson.utils.coords(geo_feature)
-        self.velocity_ms = float(velocity_ms)
+        self.velocity_meters_per_s = float(velocity_meters_per_s)
+
+    def find_rsu(self, octane_instance):
+        """
+        Given the type of messages we want to send, find a compatible RSU. Currently just
+        returns the first match. Could be extended to find nearest, etc.
+        """
+        response = octane_instance.get("/v2x/rsus")
+        response.raise_for_status()
+
+        for rsu in response.json().get("rsus", []):
+            for radio in rsu.get("radiosSupported"):
+                if radio["messageSet"] == self.protocol and radio["messageType"] == self.name \
+                      and radio["txEnabled"]:
+                    return rsu["id"]
+
+        raise RuntimeError(f"Unable to find a suitable RSU to send {self.name()}s via {self.protocol}")
 
     def path(self):
         """
@@ -158,14 +227,14 @@ class PathFollower(ABC):
                 # points, which for now will underreport velocity slightly.
                 #
                 distance_m = self.geod.line_length((prev_path_lng, path_lng), (prev_path_lat, path_lat))
-                point_count = math.ceil((distance_m / self.velocity_ms) * self.frequency_hz)
+                point_count = math.ceil((distance_m / self.velocity_meters_per_s) * self.frequency_hz)
 
                 for (point_lng, point_lat) in self.geod.npts(prev_path_lng, prev_path_lat, path_lng, path_lat,
                                                              point_count):
                     if prev_point_lat and prev_point_lng:
                         heading, _, _ = self.geod.inv(prev_point_lng, prev_point_lat, point_lng, point_lat)
 
-                        yield MovingPoint(speed=self.velocity_ms, heading=heading, coordinates=(point_lng, point_lat))
+                        yield MovingPoint(speed=self.velocity_meters_per_s, heading=heading, coordinates=(point_lng, point_lat))
 
                     prev_point_lat = point_lat
                     prev_point_lng = point_lng
@@ -173,16 +242,23 @@ class PathFollower(ABC):
             prev_path_lat = path_lat
             prev_path_lng = path_lng
 
-    def follow(self, octane_instance, rsu_id):
+    def follow(self, octane_instance, rsu_id=None):
         """
         Given the path this object represents, follow it, posting v2x messages to an OCTANE instance
         at the proper frequency
         """
+        if not rsu_id:
+            rsu_id = self.find_rsu(octane_instance)
+
+        print(f"Traversing a path, sending {self.name}s via {self.protocol} to RSU {rsu_id}")
+
         start = time.time()
         for moving_point in self.path():
             # post the v2x message, then sleep until it's time to post another
-            response = octane_instance.post_json_message(self.endpoint(rsu_id), self.as_message(moving_point))
-            response.raise_for_status()
+            data = self.as_message(moving_point)
+            print(moving_point, end="\r")
+
+            octane_instance.emit(f"v2x_{self.name}", {'id': rsu_id, 'payload': data})
 
             # Sleep for the remaining time. Right now if that's negative just continue
             elapsed = time.time() - start
@@ -190,6 +266,8 @@ class PathFollower(ABC):
             if remaining > 0:
                 time.sleep(remaining)
             start = time.time()
+
+        print("\n")
 
 
 class VehiclePathFollower(PathFollower, BSM):
@@ -207,10 +285,11 @@ class HumanPathFollower(PathFollower, PSM):
 
 
 def main():
-    octane = OctaneInstance("reticulatingsplines")
-    vpf = VehiclePathFollower("roundabout.json", 4.5)
-    vpf.follow(octane, 1)
+    with OctaneInstance("reticulatingsplines", "https://octane.mvillage.um.city") as octane:
+        vpf = VehiclePathFollower("roundabout.json", 11)
+        vpf.follow(octane)
 
 
 if __name__ == '__main__':
     main()
+
