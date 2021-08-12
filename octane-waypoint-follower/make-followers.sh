4c05661c31193a1ecb95879862e85b68e70cdcd0
hessedoneen <hessedoneen@gmail.com>
2021-08-12 10:43:48 -0400

    sending obu messages to mvillage instead

--- octane-waypoint-follower/make-followers.sh
+++ octane-waypoint-follower/make-followers.sh
@@ -10,7 +10,7 @@ echo "starting up all followers"
 
 for arg in "$@"
 do
-    ./follow-path.py aroundMCity.json --v2x-type vehicle --speed 10 --octane-server http://localhost:5000 --auth reticulatingsplines --id $arg &
+    ./follow-path.py aroundMCity.json --v2x-type vehicle --speed 10 --octane-server https://octane.mvillage.um.city --auth reticulatingsplines --id $arg &
 done
 
 echo "all followers started!"
